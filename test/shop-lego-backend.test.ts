492f98621d4b90fb3dbd26ef4b2f1888da443d48
bhupinder singh <bsin@siteimprove.com>
2025-04-20 22:32:04 -0600

    Initial commit

---
+++ test/shop-lego-backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ShopLegoBackend from '../lib/shop-lego-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/shop-lego-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ShopLegoBackend.ShopLegoBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
